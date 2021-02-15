6e33a7c770e6c5c67f36885247797ebb14b46d2b
Alan da Silva Alves <linuxfama@gmail.com>
2021-02-15 11:34:49 -0300

    Go - Novos produtos via banco de dados

--- src/alura/web/controllers/controllers.go
+++ src/alura/web/controllers/controllers.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"alura/web/models"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 // temp para chamar meus templates
@@ -13,4 +15,32 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+// New para cadrastro de protudos
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+// Insert vai inserir novos produtos no banco de dados
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço: ", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade: ", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
 }
--- src/alura/web/routes/routes.go
+++ src/alura/web/routes/routes.go
@@ -8,4 +8,6 @@ import (
 // CarregaRotas do web
 func CarregaRotas() {
 	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/new", controllers.New)
+	http.HandleFunc("/insert", controllers.Insert)
 }
