d3e7d664e53b24b4e433f3d64e086157b434bb6f
Alan da Silva Alves <linuxfama@gmail.com>
2021-02-17 23:08:33 -0300

    Go - Listas Ligadas

--- src/treinaweb/basic/aula_06/listas/main.go
+++ src/treinaweb/basic/aula_06/listas/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Criando Arrays")
+	fmt.Println("Criando Listas Ligadas:")
+	numeros := list.New()
+	el := numeros.PushBack(1)
+	numeros.PushFront(0)
+	numeros.PushBack(2)
+	for e := numeros.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	fmt.Println("-------------------")
+	numeros.Remove(el)
+	for e := numeros.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
 }
