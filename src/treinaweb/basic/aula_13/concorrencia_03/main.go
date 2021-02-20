d3e111885d641c92b688bf85266f564efdbc344b
Alan da Silva Alves <linuxfama@gmail.com>
2021-02-20 13:55:27 -0300

    Go - Concorrência - Interação entre Goroutines

---
+++ src/treinaweb/basic/aula_13/concorrencia_03/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println(" Concorrência - Programação Assíncrona")
+	fmt.Println(" Interação entre Goroutines ")
+
+	var limite int
+	fmt.Print("Informe um limite: ")
+	fmt.Scanf("%d", &limite)
+	/*
+		conteAte agora recebe uma goroutine com a palavra chave go na frente
+		Goroutines run in the same address space, so access to shared memory must be synchronized.
+	*/
+	go conteAte(limite)
+	// função anônima
+	go func(n int) {
+		for i := 0; i <= n*10; i++ {
+			fmt.Printf(" - |anônimo| O número é %d \n", i)
+		}
+	}(limite)
+	for i := 0; i <= limite*10; i++ {
+		fmt.Printf(" - |main| O número é %d \n", i)
+		/*
+			Multiplicando 10x a definição de 1 segundo.
+			Objeto é conseguir visualizar o processamento Assíncrono
+		*/
+	}
+	time.Sleep(10 * time.Second)
+}
+
+// conteAte o limite
+func conteAte(limite int) {
+	for i := 0; i <= limite*20; i++ {
+		fmt.Printf(" - |conteAte| O número é %d \n", i)
+	}
+}
