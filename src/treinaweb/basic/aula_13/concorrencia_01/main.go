aea6570b710e65dc4712542c5d35c01e874eb92f
Alan da Silva Alves <linuxfama@gmail.com>
2021-02-20 09:48:31 -0300

    Go - Vamos de Programação síncrona

---
+++ src/treinaweb/basic/aula_13/concorrencia_01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Concorrência - Programação Assíncrona")
+	fmt.Println(" 1º Vamos deProgramação síncrona")
+
+	var limite int
+	fmt.Print("Informe um limite: ")
+	fmt.Scanf("%d", &limite)
+	conteAte(limite)
+	for i := 0; i <= limite*10; i++ {
+		fmt.Printf(" - |main| O número é %d \n", i)
+	}
+}
+
+func conteAte(limite int) {
+	for i := 0; i <= limite*20; i++ {
+		fmt.Printf(" - |conteAte| O número é %d \n", i)
+	}
+}
